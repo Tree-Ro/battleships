f7788ffa4adb8f969945cd4b442361173928bd28
David Steiner <david1.steiner@gmail.com>
2024-05-28 15:36:28 +0200

    Added placement validation and completed placeShip method

--- src/game/gameboardModules/gameboard.js
+++ src/game/gameboardModules/gameboard.js
@@ -1,7 +1,7 @@
 import Ship from '../shipModules/ship.js';
 
 function createGameboard(size = 10) {
-  let gameboard = [];
+  let gameboard = generateBoard(size);
 
   function generateBoard(size) {
     let board = [];
@@ -9,21 +9,56 @@ function createGameboard(size = 10) {
     for (let x = 0; x < size; x++) {
       board[x] = [];
       for (let y = 0; y < size; y++) {
-        const cellData = {
+        let cellData = {
           hit: false,
           isShip: false,
+          shipInstance: null,
         };
         board[x].push(cellData);
       }
     }
+    return board;
   }
-  gameboard = generateBoard();
 
-  function placeShip(startCords, endCords) {
+  function placeShip(startCords, endCords, shipInstance = Ship(4)) {
     if (!validateShipPlacement(startCords, endCords)) return null;
-  }
 
+    let [x1, y1] = startCords;
+    let [x2, y2] = endCords;
+    const coordinateDelta = [x1 - x2, y1 - y2];
+
+    let length = +coordinateDelta.filter(Boolean);
+    length += Math.sign(length); //Increases delta by 1
+    let xOrientedShip = coordinateDelta[0] ? true : false;
+    for (let i = length; i !== 0; i -= Math.sign(i)) {
+      gameboard[x1][y1].isShip = true;
+      gameboard[x1][y1].shipInstance = shipInstance;
+
+      if (xOrientedShip) {
+        x1++;
+      } else {
+        y1++;
+      }
+    }
+
+    return gameboard;
+  }
   function validateShipPlacement(startCords, endCords) {
+    let [x1, y1] = startCords;
+    let [x2, y2] = endCords;
+    const coordinateDelta = [x1 - x2, y1 - y2];
+
+    let values = coordinateDelta.filter(Boolean);
+
+    //Diagonal placement or length of 0
+    if (values.length !== 1) return null;
+
+    //Out of bounds placement
+    let invalidCords = [x1, y1, x2, y2].filter((coordinate) => {
+      return coordinate > size - 1 || coordinate < 0;
+    });
+    if (invalidCords.length) return null;
+
     return true;
   }
 
--- src/game/gameboardModules/gameboard.test.js
+++ src/game/gameboardModules/gameboard.test.js
@@ -1,7 +1,26 @@
+import { experiments } from 'webpack';
 import createGameboard from './gameboard.js';
 
 test('places ships at specific coordinates correctly', () => {
   let myGameboard = createGameboard();
+
+  let currentState = myGameboard.placeShip([0, 0], [2, 0]);
+
+  expect(currentState[0][0].isShip).toBe(true);
+  expect(currentState[1][0].isShip).toBe(true);
+  expect(currentState[2][0].isShip).toBe(true);
+});
+
+test('validates illegal ship placements', () => {
+  let myGameboard = createGameboard();
+
+  const diagonalPlacement = myGameboard.placeShip([0, 0], [1, 1]);
+  const outofBounds = myGameboard.placeShip([0, 0], [-1, 0]);
+  const noSize = myGameboard.placeShip([5, 3], [5, 3]);
+
+  expect(diagonalPlacement).toBe(null);
+  expect(outofBounds).toBe(null);
+  expect(noSize).toBe(null);
 });
 
 test.todo('recieves attacks correctly');
