8ec6677d84ce7e4cb5f2a03f37f697674cd265ad
David Steiner <david1.steiner@gmail.com>
2024-05-29 16:32:06 +0200

    Added some css describing a miss on the gameboard

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import './style.css';
-import Player from './game/playerModules/player.js';
-import createGameboard from './game/gameboardModules/gameboard.js';
-import { createPlayerBoard, createNPCBoard, renderGameboard } from './dom.js';
+import { renderGameboard } from './dom.js';
 
 (() => {
   const startGameButton = document.querySelector('#introduction button');
